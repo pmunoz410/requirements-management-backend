146e4c491212c27034d7a4f72a5c72d7773ce478
Paul Muñoz <paul18a20@gmail.com>
2026-02-11 14:53:50 -0500

    Carga inicial

---
+++ src/main/java/pm/dev/code/requirements_management_backend/RequirementsManagementBackendApplication.java
@@ -0,0 +1,13 @@
+package pm.dev.code.requirements_management_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RequirementsManagementBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RequirementsManagementBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pm/dev/code/requirements_management_backend/RequirementsManagementBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pm.dev.code.requirements_management_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RequirementsManagementBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
